ab7ec4df06a53b52108050d6d1410b4a54c15b86
Filippos Zampounis <fizampou@microsoft.com>
2016-06-17 16:41:03 +0200

    adding comments and polishing the client.js

--- js/client.js
+++ js/client.js
@@ -2,12 +2,13 @@ var mosaic              = require('./mosaic.js'),
     imagePicker         = require('./imagePicker.js'),
     Coords              = require('./Coords.js'),
     imageSlicer         = require('./imageSlicer.js'),
-    tilesArray          = [],
-    dominantColorsArray = [],
-    drawingPointer      = 0,
-    workersCount        = 4,
-    pendingWorkers      = 4,
-    workersArray        = [workersCount];
+    tilesArray          = [], // Array holding tiles
+    dominantColorsArray = [], // Array holding dominant colors per tile
+    drawingPointer      = 0,  // Pointer on our drawn rows
+    workersCount        = 4,  // Number of workers
+    pendingWorkers      = workersCount,  // Counter of finished workers
+    workersArray        = [workersCount], // Array holding our workers
+    canvas              = document.getElementById('canvas'); //Canvas to draw the final image
 
 imagePicker.init(ImageLoaded);
 
@@ -16,19 +17,25 @@ function ImageLoaded (file) {
     var imageToSlice = file.target,
         noOfTilesX   = imageToSlice.width / mosaic.TILE_WIDTH,
         noOfTilesY   = imageToSlice.height / mosaic.TILE_HEIGHT,
-        imageTiles   = imageSlicer.sliceImageIntoTiles(imageToSlice, new Coords(noOfTilesX, noOfTilesY)),
-        blockSize    = Math.ceil(imageTiles.length / workersCount),
-        canvas       = document.getElementById('canvas');
+        imageTiles   = imageSlicer.sliceImageIntoTiles(imageToSlice, new Coords(noOfTilesX, noOfTilesY));
 
+    //TODO: clean the canvas on second image
+
+    // set canvas dimensions equal to image dimensions
     canvas.width = imageToSlice.width;
     canvas.height = imageToSlice.height;
 
-    for (var index = 0; index < workersCount; index++) {
+    assignToImageWorkers(imageTiles);
+}
 
+// Create the workers and assing them some tasks
+function assignToImageWorkers(imageTiles) {
+    var blockSize = Math.ceil(imageTiles.length / workersCount), // round the blocksize to the greater or equal initger number
+        index     = 0;
+
+    for (index; index < workersCount; index++) {
         workersArray[index] = new Worker('js/tileProcessor.js');
-
-        workersArray[index].onmessage = onWorkEnded;
-
+        workersArray[index].onmessage = onRowReady;
         workersArray[index].postMessage({
             data: imageTiles.slice(blockSize * index, (blockSize * index) + blockSize),
             index: index
@@ -36,7 +43,9 @@ function ImageLoaded (file) {
     }
 }
 
-function onWorkEnded (e) {
+// Save the ready rows of dominant colors and tiles to the final arrays
+// when all workers are done send for the results for drawing per row
+function onRowReady (e) {
     var tilesRow          = e.data.result,
         dominantColorsRow = e.data.colors,
         row               = e.data.row,
@@ -58,35 +67,33 @@ function onWorkEnded (e) {
     }
 }
 
-function terminateWorker(elm) {
-    elm.terminate();
-}
-
+// Draw row on canvas
 function drawRow(tiles, colors, position) {
-    var imageTileSize = new Coords(mosaic.TILE_WIDTH, mosaic.TILE_HEIGHT);
+    var imageTileSize = new Coords(mosaic.TILE_WIDTH, mosaic.TILE_HEIGHT),
+        drawPos;
 
     for (var j = 0; j < tiles.length; j++) {
-        var drawPos = new Coords(position, j).multiply(imageTileSize);
+        drawPos = new Coords(position, j).multiply(imageTileSize);
 
         drawTileOnCanva(tiles[j], colors[j], drawPos.y, drawPos.x);
     }
 }
 
-// draw on canvas
+// Draw tile on canvas with the dominant color
 function drawTileOnCanva (tile, dominantColor, positionX, positionY) {
-    var canvas  = document.getElementById('canvas'),
-        context = canvas.getContext('2d');
-
-    // Create a Data URI (prefix + base64 encoding)
-    var dominantColorData = 'data:image/svg+xml;base64,' + window.btoa(dominantColor);
-
-    var colorImage = new Image();
+    var context = canvas.getContext('2d'),
+        dominantColorData = 'data:image/svg+xml;base64,' + window.btoa(dominantColor), // Create a Data URI (prefix + base64 encoding)
+        colorImage = new Image();
 
     colorImage.onload = function() {
         context.putImageData(tile, positionX, positionY);
         context.globalCompositeOperation = 'soft-light';
         context.drawImage(colorImage, positionX, positionY, mosaic.TILE_WIDTH, mosaic.TILE_HEIGHT);
     };
-
     colorImage.src = dominantColorData;
+}
+
+// Terminate all workers
+function terminateWorker(elm) {
+    elm.terminate();
 }
